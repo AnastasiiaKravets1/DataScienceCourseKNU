a243ca7bb83e3e4dfe62ae36e18d148232bcb1a6
AnastasiiaKravets1 <116674112+AnastasiiaKravets1@users.noreply.github.com>
2023-01-20 14:45:29 +0200

    Update pmean.R

--- pmean.R
+++ pmean.R
@@ -1,1 +1,35 @@
+file <- function(directory, id = 1:332) {
+  db <- dir(directory, full.names = T) %>% map_df(read.csv)
+  return(db)
+}
 
+pmean <- function(directory, pollutant, id = 1:332){
+  db <- file(directory, id)
+  l = NULL 
+  for(i in id){
+    l = c(l, db[[pollutant]])
+  } 
+  return (mean(amount, na.rm = T))
+}
+
+complete <- function(directory, id){
+   db <- file(directory, id)
+   l = NULL
+   for (i in id) {
+     l = c(l, nrow(na.omit(myDataBase)))
+   } 
+   return(data.frame(id, l))
+}
+
+corr <- function(directory, threshold = 0){
+  db <- file(directory, id)
+  corelations = numeric()
+  
+  for (i in 1:332) {
+    l = na.omit(db)
+    if (nrow(monitor) > threshold) {
+      corelations = c(corelations, cor(l$sulfate, l$nitrate))
+    }
+  }
+  return(corelations)
+}
